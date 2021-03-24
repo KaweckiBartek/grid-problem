c4b38ed07bf176cfd07735d6eb4f1aed67b064e6
Bartlomiej Kawecki <kawec11@gmail.com>
2021-03-24 18:01:22 +0100

    Grid problem Ready

--- grid-problem/src/App.tsx
+++ grid-problem/src/App.tsx
@@ -1,9 +1,16 @@
 import Grid from "./components/grid";
+import { Helmet } from "react-helmet";
+
 
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Bartłomiej Kawecki - Grid problem</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Grid />
     </div>
   );
